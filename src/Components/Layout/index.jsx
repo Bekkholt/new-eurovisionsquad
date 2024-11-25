a638f8cc4b5eaee7db20c1a8ab8e1b2caf2daa07
Tina <tina.bekkholt@hotmail.com>
2024-11-25 13:53:10 +0100

    added home to layout

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "../Header";
+import Home from "../../Pages/Home";
 
 export default function Layout() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
     </div>
   );
 }
