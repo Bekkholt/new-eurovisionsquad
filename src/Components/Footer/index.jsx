f9ef32f7196578f28589fbcaac786438886e7bfe
Tina <tina.bekkholt@hotmail.com>
2024-11-29 12:15:01 +0100

    added bootstrap stylesheet

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div className="d-flex justify-content-center bg-primary">
+      <div>
+        <p className="d-flex justify-content-center">Nettside fra:</p>
+        <a href="https://www.bekkholt.no/">
+          <p className="d-flex justify-content-center">Bekkholt</p>
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "../Header";
 import Home from "../../Pages/Home";
+import Footer from "../Footer";
 
 export default function Layout() {
   return (
@@ -9,6 +10,7 @@ export default function Layout() {
       <Routes>
         <Route index element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
