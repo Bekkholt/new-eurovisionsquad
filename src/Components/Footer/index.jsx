883c080449054fe5a1f1e08b54f85246688f9967
Tina <tina.bekkholt@hotmail.com>
2024-11-29 12:19:22 +0100

    fixed bottom footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="d-flex justify-content-center bg-primary">
+    <div className="d-flex justify-content-center bg-primary fixed-bottom">
       <div>
         <p className="d-flex justify-content-center">Nettside fra:</p>
         <a href="https://www.bekkholt.no/">
